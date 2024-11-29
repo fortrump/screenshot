9ffba1719a7df10ab86ccdc82927bbe4ea3382d9
gprethesh <georgeprethesh@georges-MacBook-Air.local>
2024-11-29 12:27:52 +0530

    added for cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,31 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+import cloudflare from "@astrojs/cloudflare";
 import react from "@astrojs/react";
-
 import sitemap from "@astrojs/sitemap";
 import { CONFIG, LANGUAGES_CODE } from "./src/lib/config";
 
-// https://astro.build/config
 export default defineConfig({
   site: CONFIG.website,
-  trailingSlash: 'ignore',
+  trailingSlash: "ignore",
   compressHTML: false,
   i18n: {
     defaultLocale: "en",
     locales: CONFIG.locals,
     routing: {
-        prefixDefaultLocale: false
-    }
+      prefixDefaultLocale: false,
+    },
   },
-  integrations: [tailwind(), react(), sitemap({
-    i18n: {
-      defaultLocale: "en",
-      locales: LANGUAGES_CODE
-    }
-  })],
+  integrations: [
+    tailwind(),
+    react(),
+    sitemap({
+      i18n: {
+        defaultLocale: "en",
+        locales: LANGUAGES_CODE,
+      },
+    }),
+  ],
   output: "server",
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true
-    }
-  })
+  adapter: cloudflare(),
 });
